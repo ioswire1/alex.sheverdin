f07c6f51a31fc47ea620421c86378621188c762b
Alex Sheverdin <alex.sheverdin@gmail.com>
2015-10-08 14:44:42 +0300

    Before color animation alteration

--- WeatherApp/CircleView.m
+++ WeatherApp/CircleView.m
@@ -54,7 +54,8 @@ static double temperatureMax = 50.0;
     if (temperature < -temperatureMax)
         temperature = -temperatureMax;
     _temperature = temperature;
-    
+
+
     CGRect bounds = self.bounds;
     CGPoint center;
     center.x = bounds.origin.x + bounds.size.width / 2.0;
@@ -78,37 +79,55 @@ static double temperatureMax = 50.0;
   
     //create animation layer
     UIBezierPath *path = [[UIBezierPath alloc] init];
-    CGFloat endAngle = self.temperature * 360 / 50;
-    CGFloat startAngle = prevTemperature * 360 / 50;
+//    CGFloat endAngle = self.temperature * 360 / temperatureMax;
+//    CGFloat startAngle = prevTemperature * 360 / temperatureMax;
     [path addArcWithCenter:center
                     radius:radius - self.lineWidth / 2
                 startAngle:DegreesToRadians(self.initAngle)
-                  endAngle:DegreesToRadians(self.initAngle + endAngle)
+                  endAngle:DegreesToRadians(360)
+//                  endAngle:DegreesToRadians(self.initAngle + endAngle)     
 //                startAngle:DegreesToRadians(self.initAngle + startAngle)
 //                  endAngle:DegreesToRadians(self.initAngle + endAngle)
-                 clockwise:self.temperature >= prevTemperature];
+                 clockwise:self.temperature];
     path.lineWidth = self.lineWidth;
     self.circleLayer.path = path.CGPath;
     self.circleLayer.position = bounds.origin;
     self.circleLayer.fillColor = [UIColor clearColor].CGColor;
     self.circleLayer.lineWidth = self.lineWidth;
     [self.layer addSublayer:self.circleLayer];
+    
+    NSMutableArray *colors = [[self colors] mutableCopy];
+    if (nil != (__bridge id _Nonnull)([self.circleLayer.presentationLayer strokeColor])) {
+        colors[0] = (__bridge id _Nonnull)([self.circleLayer.presentationLayer strokeColor]);
+    }
+
+    
+//    CGColorRef tempColor = self.backLayer.backgroundColor;
+//    self.backLayer.strokeColor = [self.circleLayer.presentationLayer strokeColor];
+//    tempColor = [self.circleLayer.presentationLayer strokeColor];
 
     CABasicAnimation *drawAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     drawAnimation.duration  = self.duration;
-    drawAnimation.fromValue = @(0.0f);
-    drawAnimation.toValue   = @(1.0f);
+//    drawAnimation.fromValue = @(0.5f);
+//    drawAnimation.toValue   = @(1.0f);
+    drawAnimation.fromValue = @(prevTemperature / temperatureMax);
+    drawAnimation.toValue   = @(_temperature / temperatureMax);
+
+    drawAnimation.fillMode = kCAFillModeForwards;
+    drawAnimation.removedOnCompletion = NO;
     drawAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     [self.circleLayer addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
     
     CAKeyframeAnimation *colorAnimation = [CAKeyframeAnimation animationWithKeyPath:@"strokeColor"];
     colorAnimation.duration = self.duration;
-    colorAnimation.values   = [self colors];
+
+    colorAnimation.values   = colors;
     colorAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     colorAnimation.fillMode = kCAFillModeForwards;
     colorAnimation.removedOnCompletion = NO;
-    CGFloat koef = fabs(temperatureMax / _temperature);
-    NSArray *times = @[@(0.0f * koef), @(0.25f * koef), @(0.5f * koef), @(0.75 * koef), @(1.0f * koef)];
+    double koef = fabs(temperatureMax / _temperature);
+    double delta = prevTemperature / temperatureMax;
+    NSArray *times = @[@(0.0f * koef - delta), @(0.25f * koef - delta), @(0.5f * koef - delta), @(0.75 * koef - delta), @(1.0f * koef - delta)];
     [colorAnimation setKeyTimes:times];
     [self.circleLayer addAnimation:colorAnimation forKey:@"colorCircleAnimation"];
 }
