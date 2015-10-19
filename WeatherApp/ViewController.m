a84f36462b035ac4e2756f6c2c19e19e6dae8116
Halkyonbird <alex.sheverdin@gmail.com>
2015-10-19 09:23:50 +0300

    added loaded temperature as source for animation

--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -17,6 +17,8 @@
 @property (strong, nonatomic) UIDynamicAnimator *animator;
 @property (strong, nonatomic) FallBehavior *fallBehavior;
 @property (weak, nonatomic) IBOutlet UISlider *progressValue;
+@property (weak, nonatomic) IBOutlet UILabel *lblTemperature;
+@property (strong, nonatomic) NSNumber *temperature;
 
 @end
 
@@ -39,6 +41,7 @@
     [self.circleView addProgressAnimation:-50.0 completion:^(BOOL finished) {
         [self addLoadAnimationWithBounceCount:3];
     }];
+    [self downloadWeather];
 }
 
 
@@ -66,10 +69,10 @@
         static int count = 0;
         static CGFloat xPrev = 0, xPrevDelta = 0;
         CGFloat xCurrentDelta = wSelf.circleView.center.y - xPrev;
-        NSLog(@"curY = %.1f prevY = %.1f xCurrentDelta = %.1f xPrevDelta = %.1f", wSelf.circleView.center.y, xPrev, xCurrentDelta, xPrevDelta );
+        //NSLog(@"curY = %.1f prevY = %.1f xCurrentDelta = %.1f xPrevDelta = %.1f", wSelf.circleView.center.y, xPrev, xCurrentDelta, xPrevDelta );
         if ((xCurrentDelta < 0) && (xPrevDelta >= 0)) {
             count++;
-            NSLog(@"Direction changed!");
+            //NSLog(@"Direction changed!");
         }
         if (count >= bounceCount) {
             
@@ -77,7 +80,8 @@
                 [wSelf.fallBehavior removeItem:wSelf.circleView];
                 wSelf.circleView.center = wSelf.view.center;//CGPointMake(30.0,30.0);
             } completion:^(BOOL finished) {
-                [wSelf.circleView addProgressAnimation:wSelf.progressValue.value completion:nil];
+//                [wSelf.circleView addProgressAnimation:wSelf.progressValue.value completion:nil];
+                [wSelf.circleView addProgressAnimation:[wSelf.temperature doubleValue] completion:nil];
             }];
             count = 0;
             xPrev = xPrevDelta = 0.0;
@@ -88,12 +92,45 @@
     };
 }
 
+#pragma mark - Getting Weather Data
+
+- (void) downloadWeather {
+//    if ((0 == [self currentLocation].coordinate.latitude) && (0 == [self currentLocation].coordinate.longitude)) {
+//        self.lblFailedLocation.hidden = NO;
+//        return;
+//    } else {
+//        self.lblFailedLocation.hidden = YES;
+//    }
+    
+    OpenWeatherMap *weatherService = [OpenWeatherMap service];
+    
+    [weatherService getWeatherForCityName:@"kharkov" completion:^(NSDictionary * dictionary, NSError * error) {
+        NSDictionary *main = dictionary[@"main"];
+        self.temperature =  main[@"temp"];
+        self.lblTemperature.text = [NSString stringWithFormat:@"%@",self.temperature];
+        NSLog(@"temp = %@", self.temperature);
+//    [weatherService getWeatherForLocation:self.currentLocation.coordinate completion:^(NSDictionary * dictionary, NSError * error) {
+
+//        if (error) {
+//            self.lblFailedConnection.hidden = NO;
+//        } else {
+//            Weather *weather = [Weather weatherWithDictionary:dictionary inContext:[self managedObjectContext]];
+//            self.lblFailedConnection.hidden = YES;
+//            [self showWeather:weather];
+//            if (![[self managedObjectContext] save:&error]) {
+//                //NSLog(@"%@", error);
+//            }
+//        }
+    }];
+}
+
 
 #pragma mark - Lifecycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self downloadWeather];
     [self addLoadAnimationWithBounceCount:5];
 }
 
