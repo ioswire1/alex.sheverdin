2eb89ba94613765fe119e4a50451ba46a16c328a
Alex Sheverdin <alex.sheverdin@gmail.com>
2015-11-04 19:17:26 +0200

    draft of gradient filled trend implemented

---
+++ WeatherApp/TUTSimpleScatterPlot.h
@@ -0,0 +1,13 @@
+//
+//  TUTSimpleScatterPlot.h
+//  WeatherApp
+//
+//  Created by Alex Sheverdin on 11/4/15.
+//  Copyright © 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TUTSimpleScatterPlot : NSObject
+
+@end
---
+++ WeatherApp/TUTSimpleScatterPlot.m
@@ -0,0 +1,13 @@
+//
+//  TUTSimpleScatterPlot.m
+//  WeatherApp
+//
+//  Created by Alex Sheverdin on 11/4/15.
+//  Copyright © 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import "TUTSimpleScatterPlot.h"
+
+@implementation TUTSimpleScatterPlot
+
+@end
