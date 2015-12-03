f9cbff02eb8f7553ea7bc6087219c94728ba0243
Alex Sheverdin <alex.sheverdin@gmail.com>
2015-12-03 21:17:25 +0200

    Array of city objects was added

---
+++ WeatherApp/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  WeatherApp
+//
+//  Created by Alex Sheverdin on 12/3/15.
+//  Copyright © 2015 Alex Sheverdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
